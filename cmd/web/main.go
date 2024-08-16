e3f6c7865a3e04c84893b0b3874e7ee683fd1a96
MQuang <65891056+quang2652001@users.noreply.github.com>
2024-08-16 22:07:25 +0700

    create file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,12 +6,17 @@ import (
 )
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
-    mux.HandleFunc("/snippet/create", snippetCreate)
+  
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+  
+  mux := http.NewServeMux()
+  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+  mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
+
+  log.Println("Starting server on :4000")
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
 }

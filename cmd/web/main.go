8ae89fc4f6f011cb1f93406d4b7107911d7a7149
MQuang <65891056+quang2652001@users.noreply.github.com>
2024-09-24 19:35:25 +0700

    format main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "flag"
-    "os"
+	"flag"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 
-  addr := flag.String("addr", ":4000", "HTTP network address")
-  flag.Parse()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-  errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-  
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-  
-  mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-  srv := &http.Server{
-    Addr: *addr,
-    ErrorLog: errorLog,
-    Handler:  mux,
-  }
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
-  infoLog.Printf("Starting server on %s", *addr)
-  err := srv.ListenAndServe()
-  errorLog.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
